d7f7a04d5dc946cce501bf360a7ae4016a2cc174
Duilio Felix <duiliofelix@gmail.com>
2023-01-26 11:00:15 -0300

    adicionei javascript

---
+++ main.js
@@ -0,0 +1,23 @@
+
+let meuTexto = "testa"
+
+let meuNumero = 12453
+
+let meuBooleano = false
+
+let minhaLista = ["a", "b", "c", "d", "e", "f"]
+
+let meuObjeto = {
+    nome: "texto",
+    idade: 20
+}
+
+
+alert(meuTexto)
+alert(meuNumero)
+alert(meuBooleano)
+alert(minhaLista[4])
+alert(meuObjeto.nome)
+
+// descobrir o que if e for fazem
+// armazenar o valor do texto digitado na barrinha de busca em uma variavel
